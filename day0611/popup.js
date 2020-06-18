7dc2ed4b3ee7e40726bd0b4602a08c195c284ddb
toraring <65555521+toraring@users.noreply.github.com>
2020-06-18 14:46:33 +0900

    Add files via upload

---
+++ day0611/popup.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+//웹브라우져 새창열 때 가장 간단(url, 이름, 스펙)
+function pop1()
+{
+window.open('../Image/a1.jpg','Event',
+		'width = 430, height = 430, scrollbar = yes, status = no');
+}
+function pop2() 
+{
+window.open('http://www.na ver.com','Event',
+'width = 430, height = 430, scrollbar = yes, status = no');	
+}
